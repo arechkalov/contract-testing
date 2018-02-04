b82030b122c95214a89b20ad07dc3cb326738b7c
arechkalov <antony.rechkalov@gmail.com>
2018-02-04 10:13:01 +0200

    added FraudBase for contract tests

---
+++ fraud-service/src/test/java/com/endava/fraud/FraudBase.java
@@ -0,0 +1,16 @@
+package com.endava.fraud;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.Before;
+
+public class FraudBase {
+
+    @Before
+    public void setup() {
+        RestAssuredMockMvc.standaloneSetup(new FraudDetectionController());
+    }
+
+	public void assertThatRejectionReasonIsNull(Object rejectionReason) {
+		assert rejectionReason == null;
+	}
+}
